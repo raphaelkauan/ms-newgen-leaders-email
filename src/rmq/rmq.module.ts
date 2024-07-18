02aee47e205f21ba4fcd23cb8ab18a518a779267
raphaelkauan <raphaelkauanoficial@gmail.com>
2024-07-17 20:37:28 -0400

    feat: adding validation for email

--- src/rmq/rmq.module.ts
+++ src/rmq/rmq.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { RmqService } from './rmq.service';
 import { RmqController } from './rmq.controller';
 import { MailerModule } from 'src/mailer/mailer.module';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
     imports: [MailerModule],
--- src/rmq/rmq.service.ts
+++ src/rmq/rmq.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { MailDto } from './dto/mailDto';
 import { MailerService } from 'src/mailer/mailer.service';
 import { PrismaClient } from '@prisma/client';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class RmqService {
@@ -18,10 +18,17 @@ export class RmqService {
             html: `<h1>Bem vindo(a) <strong>${data.username}</strong>!</h1>`,
         };
 
+        const email = await this.prisma.users.findFirst({ where: { email: data.email } });
+
+        if (email) {
+            console.log('Esse email já existe.');
+            return;
+        }
+
         await this.prisma.users.create({ data });
 
         console.log(data);
 
-        this.mailerService.sendMail(message);
+        await this.mailerService.sendMail(message);
     }
 }
